b303de8edab9c2df7ab48ea165d64fffcc2164c8
Melis Cagan <meliscagan@gmail.com>
2020-02-05 17:18:12 +0000

    initial commit with all my work so far

---
+++ number-to-string.js
@@ -0,0 +1,3 @@
+n = 128
+m = n.toString()
+console.log(m)
---
+++ objects.js
@@ -0,0 +1,155 @@
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+v
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni' ],
+  crust: 'deep dish',
+  serves: 2
+}
+console.log(pizza)
